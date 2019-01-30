22bbcf5479afb72644906d99f0b4e06501bb6e1f
Rayan Mohamed <rayan.mohamed@centrica.com>
2019-01-30 00:24:10 +0000

    Add user registration route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  **********************************************/
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = require("./constants").PORT;
 const app = express();
 
@@ -14,10 +15,14 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
+// use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-// Start express server
+// start express server
 app.listen(PORT, console.log(`App running on PORT: ${PORT}`));
