ccc155e626c63568ae32f0407827dd323bc7b13c
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-08-26 21:34:07 +0100

    Remove redundant code

--- constants.js
+++
@@ -1,8 +0,0 @@
-/**
- * Defines the port the application serves HTTP responses on
- * Set port to 3000 if process.env.PORT is not defined
- */
-const PORT = process.env.PORT || 5000;
-
-// Export constants
-module.exports.PORT = PORT;
