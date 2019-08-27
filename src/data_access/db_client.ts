8ce0547488d7f5e8ae38a7a6cff067b76cc41a77
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-08-27 01:02:06 +0100

    Add mongo connection

---
+++ src/data_access/db_client.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { MongoCallback, MongoClient } from 'mongodb';
+
+const MONGO_URI: string = `${process.env.MONGO_URI}`
+
+const getMongoDbClient = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+    console.log('Successfully connected to DbClient');
+  }
+  catch(error) {
+    console.error(`Failed to connect to DbClient: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+export {getMongoDbClient}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { getMongoDbClient } from './data_access/db_client';
 
 const PORT = process.env.PORT || '5000';
-const app: express.Application  = express();
+
+const app: express.Application  = express(); // initialise express application
+getMongoDbClient(); // connect to MongoDB Client
+
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({"hellow": "world"});
