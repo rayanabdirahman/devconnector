a99c38ec91cf90fd8bef83e64a9a2655140b2e46
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-08-27 01:40:57 +0100

    Add boilerplate routes

---
+++ src/api/profile/profile.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const router: express.Router = express.Router();
+
+/**
+ * @get api/users
+ * @description Test route
+ * @public
+ */
+router.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Profile api');
+});
+
+export { router };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { getMongoDbClient } from './data_access/db_client';
+import { router as auth } from './api/auth/auth';
+import { router as post } from './api/post/post';
+import { router as profile } from './api/profile/profile';
+import { router as user } from './api/user/user';
 
 const PORT = process.env.PORT || '5000';
 
 const app: express.Application  = express(); // initialise express application
-getMongoDbClient(); // connect to MongoDB Client
+getMongoDbClient(); // connect to MongoDB client
 
+app.use('/api/auth', auth); // use authentication route
+app.use('/api/post', post); // use post route
+app.use('/api/profile', profile); // use profile route
+app.use('/api/user', user); // use route route
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({"hellow": "world"});
