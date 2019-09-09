c0ff9c4c394bb2ce8646190fe1edfbd77e2cdb67
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-09-09 19:50:05 +0100

    Add profile model

---
+++ src/data_access/models/profile.model.ts
@@ -0,0 +1,40 @@
+import { ProfileModel } from '../../domain/interfaces';
+import mongoose, { Schema } from 'mongoose';
+
+const ProfileSchema: Schema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  company: { type: String },
+  website: { type: String },
+  location: { type: String },
+  status: { type: String, required: true },
+  skills: { type: [String], required: true },
+  bio: { type: String },
+  githubUserName: { type: String },
+  experience: [{
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    location: { type: String },
+    from: { type: Date, required: true },
+    to: { type: Date },
+    current: { type: Boolean, default: false },
+    description: { type: String }
+  }],
+  education: [{
+    school: { type: String, required: true },
+    degree: { type: String, required: true },
+    fieldOfStudy: { type: String },
+    from: { type: Date, required: true },
+    to: { type: Date },
+    current: { type: Boolean, default: false },
+    description: { type: String }
+  }],
+  social: {
+    youtube: { type: String },
+    twitter: { type: String },
+    facebook: { type: String },
+    linkedIn: { type: String },
+    instagram: { type: String },
+  }
+});
+
+export default mongoose.model<ProfileModel>('Profile', ProfileSchema); 
--- src/domain/interfaces.ts
+++ src/domain/interfaces.ts
@@ -20,3 +20,45 @@ export interface LoginModel {
   email: string;
   password: string;
 }
+
+export interface ProfileModel extends mongoose.Document {
+  user: object;
+  company?: string;
+  website?: string;
+  location?: string;
+  status: string;
+  skills: string[];
+  bio?: string;
+  githubUserName?: string;
+  expreience: Experience[];
+  education: Education[];
+  social?: SocialMedia;
+}
+
+interface Experience {
+  title: string;
+  company: string;
+  location: string;
+  from: Date;
+  to?: Date;
+  current: boolean;
+  description?: string;
+}
+
+interface Education {
+  school: string;
+  degree: string;
+  fieldOfStudy: string;
+  from: Date;
+  to?: Date;
+  current: boolean;
+  description?: string;
+}
+
+interface SocialMedia {
+  youtube?: string;
+  twitter?: string;
+  facebook?: string;
+  linkedIn?: string;
+  instagram?: string;
+}
