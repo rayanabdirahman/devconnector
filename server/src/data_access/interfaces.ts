a70f38b9735f6d7709cf5ad7a9cac8325c0aad91
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-04-02 14:58:01 +0100

    Add profile model

--- server/src/data_access/interfaces.ts
+++ server/src/data_access/interfaces.ts
@@ -16,3 +16,47 @@ export interface LoggedInUserModel {
   avatar: string;
   date: Date;
 }
+
+
+export interface ProfileModel extends mongoose.Document {
+  user: mongoose.Schema.Types.ObjectId;
+  company?: string;
+  website?: string;
+  location?: string;
+  status: string;
+  skills: [string],
+  bio?: string;
+  githubusername?: string;
+  experience: [UserExperience];
+  education: [UserEducation];
+  social: UserSocialMedia;
+  date: Date
+};
+
+export interface UserExperience {
+  title: string;
+  company: string;
+  location?: string;
+  from: Date;
+  to: Date;
+  current: boolean,
+  description?: string
+}
+
+export interface UserEducation {
+  school: string;
+  degree: string;
+  fieldofstudy: string;
+  from: Date;
+  to: Date;
+  current: boolean,
+  description?: string
+}
+
+export interface UserSocialMedia {
+  youtube?: string;
+  twitter?: string;
+  facebook?: string;
+  linkedin?: string;
+  instagram?: string;
+}
---
+++ server/src/data_access/models/profile.model.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import { ProfileModel } from '../interfaces';
+
+const ProfileSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  company: { type: String },
+  website: { type: String },
+  location: { type: String },
+  status: { type: String, required: true },
+  skills: { type: [String], required: true },
+  bio: { type: String },
+  githubusername: { type: String },
+
+  experience: [{
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    location: { type: String },
+    from: { type: Date, required: true },
+    to: { type: Date },
+    current: { type: Boolean, default: false },
+    description: { type: String }
+  }],
+
+  edication: [{
+    school: { type: String, required: true },
+    degree: { type: String, required: true },
+    fieldofstudy: { type: String, required: true },
+    from: { type: Date, required: true },
+    to: { type: Date },
+    current: { type: Boolean, default: false },
+    description: { type: String }
+  }],
+
+  social: {
+    youtube: { type: String },
+    twitter: { type: String },
+    facebook: { type: String },
+    linkedin: { type: String },
+    instagram: { type: String },
+  },
+
+  date: { type: Date, default: Date.now }
+
+});
+
+export default mongoose.model<ProfileModel>('Profile', ProfileSchema);
