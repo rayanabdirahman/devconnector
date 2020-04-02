ce0eaac72b58d1eb6577204a27e59f53ed5f7cb0
rayanabdirahman <rayanmabdirahman@gmail.com>
2020-04-02 15:20:11 +0100

    bind profile controller to inversify

--- server/src/inversify.config.ts
+++ server/src/inversify.config.ts
@@ -7,12 +7,14 @@ import { UserService, UserServiceImpl } from "./services/user.service";
 import { UserRepository, UserRepositoryImpl } from "./data_access/repositories/user.repository";
 import AccountController from "./api/account/account.controller";
 import { AccountService, AccountServiceImpl } from "./services/account.service";
+import ProfileController from "./api/profile/prodile.controller";
 
 const container = new Container();
 
 // controllers
 container.bind<RegistrableController>(TYPES.Controller).to(UserController)
 container.bind<RegistrableController>(TYPES.Controller).to(AccountController)
+container.bind<RegistrableController>(TYPES.Controller).to(ProfileController)
 
 // servies
 container.bind<UserService>(TYPES.UserService).to(UserServiceImpl);
